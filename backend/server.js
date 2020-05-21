e8a5014224eff13cbe003ca417ab95a5cc08cc0a
Lekkyjay <lekkyjayjay@yahoo.com>
2020-05-21 22:57:24 +0200

    Established connection to mongoDB_Atlas

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,19 @@
 const express = require('express');
-const app = express();
-const port = process.env.PORT || 5000;
+const mongoose = require('mongoose');
 
+require('dotenv').config();
+
+const app = express();
+
+const url = process.env.ATLAS_URL;
+mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
