d655385b3d324ef58ef12018b5a1881fcb41ae7b
Lekkyjay <lekkyjayjay@yahoo.com>
2020-05-22 23:23:37 +0200

    Implemented API endpoints for users & exercises
    Routes folder created
    Tested all endpoints with postman

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 require('dotenv').config();
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 const url = process.env.ATLAS_URL;
 mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
@@ -12,6 +16,13 @@ const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 });
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
